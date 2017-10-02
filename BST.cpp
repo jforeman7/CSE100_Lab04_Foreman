6e56292b65e0934016aa947955b1a8e9f472d387
jforeman7 <31979772+jforeman7@users.noreply.github.com>
2017-10-02 15:44:36 -0700

    Reformatted code.

--- BST.cpp
+++ BST.cpp
@@ -4,67 +4,73 @@
 
 using namespace std;
 
-/****************************************************************
- * CONSTRUCTOR
- *   Creates an empty binary tree
- ****************************************************************/
-BST::BST() {
+BST::BST() 
+{
+        
 }
 
-/****************************************************************
- * DESTRUCTOR
- *   Free all memory used by current tree
- ****************************************************************/
-BST::~BST() {
-  // Write code to remove and deallocate all nodes in the tree
+BST::~BST() 
+{
+        
 }
 
-void BST::Insert(int toInsert) {
-  // Write your code here
+void BST::Insert(int toInsert) 
+{
+        
 }
 
-void BST::Delete(int toDelete) {
-  // Write your code here
+void BST::Delete(int toDelete) 
+{
+        
 }
 
-void BST::Transplant(Node *u, Node *v) {
-  // Write your code here
+void BST::Transplant(Node *u, Node *v) 
+{
+        
 }
 
-Node *BST::Successor(Node *x) {
-  // Write your code here
+Node *BST::Successor(Node *x) 
+{
+        
 }
 
-Node *BST::Minimum(Node *x) {
-  // Write your code here
+Node *BST::Minimum(Node *x) 
+{
+        
 }
 
-Node *BST::Maximum(Node *x) {
-  // Write your code here
+Node *BST::Maximum(Node *x) 
+{
+        
 }
 
-Node *BST::Search(int toFind) {
-  // Write your code here
+Node *BST::Search(int toFind) 
+{
+        
 }
 
-void BST::Print(TraversalOrder Order) {
-  if(Order==InOrderTrav)
-    InOrder(root);
-  else if(Order==PreOrderTrav)
-    PreOrder(root);
-  else if(Order==PostOrderTrav)
-    PostOrder(root);
+void BST::Print(TraversalOrder Order) 
+{
+        if(Order==InOrderTrav)
+                InOrder(root);
+        else if(Order==PreOrderTrav)
+                PreOrder(root);
+        else if(Order==PostOrderTrav)
+                PostOrder(root);
 }
 
-void BST::PreOrder(Node *x) {
-  // Write your code here
+void BST::PreOrder(Node *x) 
+{
+        
 }
 
-void BST::InOrder(Node *x) {
-  // Write your code here
+void BST::InOrder(Node *x) 
+{
+        
 }
 
-void BST::PostOrder(Node *x) {
-  // Write your code here
+void BST::PostOrder(Node *x) 
+{
+        
 }
 
